0808bdf32ab58372bf420f5b7e437e334bab6bd2
Jo Lily <lily@example.com>
2025-10-15 22:37:21 +0900

    サーバー：CORS設定を追加

--- src/server/main.go
+++ src/server/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"connectrpc.com/connect"
-	gRPC "github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1"
 	greetv1 "github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1"
 	"github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1/gRPCconnect"
 )
@@ -15,19 +14,32 @@ type GreetServer struct{}
 
 func (s *GreetServer) Hello(
 	ctx context.Context,
-	req *connect.Request[gRPC.HelloRequest],
-) (*connect.Response[gRPC.HelloResponse], error) {
+	req *connect.Request[greetv1.HelloRequest],
+) (*connect.Response[greetv1.HelloResponse], error) {
 	res := connect.NewResponse(&greetv1.HelloResponse{
 		Message: fmt.Sprintf("Hello %s from Server!", req.Msg.Name),
 	})
 	return res, nil
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, connect-protocol-version")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	greeter := &GreetServer{}
 	mux := http.NewServeMux()
 	path, handler := gRPCconnect.NewGreetingServiceHandler(greeter)
-	mux.Handle(path, handler)
+	mux.Handle(path, corsMiddleware(handler))
 	p := new(http.Protocols)
 	p.SetHTTP1(true)
 
