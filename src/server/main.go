27c413cad7f22c0abf3eed416208f53c451df771
Jo Lily <lily@example.com>
2025-10-15 21:44:49 +0900

    サーバーの実装

--- src/server/main.go
+++ src/server/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"net/http"
 
+	"connectrpc.com/connect"
+	gRPC "github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1"
+	greetv1 "github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1"
+	"github.com/Calvados-1997/Calc_RPC/pkg/gRPC/helloapp/v1/gRPCconnect"
+)
+
+type GreetServer struct{}
+
+func (s *GreetServer) Hello(
+	ctx context.Context,
+	req *connect.Request[gRPC.HelloRequest],
+) (*connect.Response[gRPC.HelloResponse], error) {
+	res := connect.NewResponse(&greetv1.HelloResponse{
+		Message: fmt.Sprintf("Hello %s from Server!", req.Msg.Name),
+	})
+	return res, nil
+}
+
+func main() {
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	path, handler := gRPCconnect.NewGreetingServiceHandler(greeter)
+	mux.Handle(path, handler)
+	p := new(http.Protocols)
+	p.SetHTTP1(true)
+
+	p.SetUnencryptedHTTP2(true)
+	s := http.Server{
+		Addr:      "localhost:8080",
+		Handler:   mux,
+		Protocols: p,
+	}
+	fmt.Printf("Start server on %s", s.Addr)
+	s.ListenAndServe()
 }
